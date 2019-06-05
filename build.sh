1f6d14fd93a981c96e4dedbeb2dd8498aba6ff2f
Auston Stewart <auston@shedbuilt.net>
2019-06-04 16:58:56 -1000

    Initial commit of sdl2

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_KMSDRM_OPTION='disable'
+SHED_PKG_LOCAL_WAYLAND_OPTION='disable'
+SHED_PKG_LOCAL_GLES_OPTION='disable'
+SHED_PKG_LOCAL_DUMMY_VIDEO_OPTION='enable'
+for SHED_PKG_LOCAL_OPTION in "${!SHED_PKG_LOCAL_OPTIONS[@]}"; do
+    case "$SHED_PKG_LOCAL_OPTION" in
+        kmsdrm)
+            SHED_PKG_LOCAL_KMSDRM_OPTION='enable'
+            SHED_PKG_LOCAL_DUMMY_VIDEO_OPTION='disable'
+            ;;
+        wayland)
+            SHED_PKG_LOCAL_WAYLAND_OPTION='enable'
+            SHED_PKG_LOCAL_DUMMY_VIDEO_OPTION='disable'
+            ;;
+        gles)
+            SHED_PKG_LOCAL_GLES_OPTION='enable'
+            SHED_PKG_LOCAL_DUMMY_VIDEO_OPTION='disable'
+            ;;
+    esac
+done
+./configure --prefix=/usr \
+            --enable-alsa \
+	    --disable-pulseaudio \
+	    --disable-video-mir \
+	    --disable-video-x11 \
+	    --disable-video-rpi \
+	    --${SHED_PKG_LOCAL_KMSDRM_OPTION}-video-kmsdrm \
+	    --${SHED_PKG_LOCAL_WAYLAND_OPTION}-video-wayland \
+	    --${SHED_PKG_LOCAL_GLES_OPTION}-video-opengles \
+	    --disable-video-vulkan &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Remove static libraries
+rm -v "${SHED_FAKE_ROOT}"/usr/lib/libSDL2*.a
